699fe85c804ec1b3f65b4040b6746fe8075d6b24
Nurlan Amrahov <nurlan.amrahov@ailab.az>
2025-01-04 15:43:38 +0400

    IsTwoArrayPermutation

---
+++ src/array/ispermutation/IsTwoArrayPermutation.java
@@ -0,0 +1,8 @@
+package array.ispermutation;
+
+public class IsTwoArrayPermutation {
+    public static void main(String[] args) {
+        //there are two arrays, check if they are permutation wach other
+        //to do this find sum and product all elements of both of them and compare
+    }
+}
